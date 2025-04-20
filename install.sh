4a1230095ee4c3e9a97f79ff0c7712d59cfc9af4
KITANAKA, Eiji <eiji.kitanaka@gmail.com>
2025-04-20 11:36:29 +0900

    commit

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 DOT_DIR="$HOME/dotfiles"
 
-git clone https://github.com/gogotealove/dotfiles ${DOT_DIR}
-
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.config ~/.config
 
 ln -sf $DOT_DIR/.zshrc ~/.zshrc
+
+. ~/.zshrc
